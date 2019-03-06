756eb1d9ca75465c9fed871bedfcdcb082f8f04c
denemem <720694-Ogle@DESKTOP-3HSG3TM>
2019-03-06 20:15:11 +0300

    proje oluşturuldu

---
+++ Bilet.Entity/Identity/ApplicationUser.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bilet.Entity.Identity
+{
+    public class ApplicationUser:IdentityUser
+    {
+        [Required]
+        [StringLength(50)]
+        public string Ad { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Soyad { get; set; }
+    }
+}
